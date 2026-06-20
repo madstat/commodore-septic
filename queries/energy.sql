ea8db2eb130c9cb45d0172c8d5da492cd083ca55
madstat <bobbyyuen@gmail.com>
2026-06-20 10:36:10 -0700

    remove ids from energy.sql

--- queries/energy.sql
+++ queries/energy.sql
@@ -6,8 +6,6 @@ SELECT COUNT(*) FROM main.energy_history;
 
 SELECT * FROM main.energy_history WHERE datetime = '2026-06-16 00:00:00'; LIMIT 10;
 
-SELECT * FROM main.bookings;
 
-SELECT * FROM main.bookings WHERE booking_id = '20270086';
 
 
